1a112a73ba04c64614cb72e1e0b77b19115d66d0
m.reza <mohammadrezakhodarahmi1382@gmail.com>
2023-03-29 14:40:53 +0430

    Add project files.

---
+++ eight_queen_problem/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Eight_queen_problem
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_solve = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.DataGridView();
+            this.time_txtbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.output)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click the Button to solve the Eight Queen problem";
+            // 
+            // btn_solve
+            // 
+            this.btn_solve.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_solve.Location = new System.Drawing.Point(522, 9);
+            this.btn_solve.Name = "btn_solve";
+            this.btn_solve.Size = new System.Drawing.Size(75, 27);
+            this.btn_solve.TabIndex = 2;
+            this.btn_solve.Text = "Solve";
+            this.btn_solve.UseVisualStyleBackColor = true;
+            this.btn_solve.Click += new System.EventHandler(this.btn_solve_Click);
+            // 
+            // output
+            // 
+            this.output.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.output.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.output.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.output.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.output.DefaultCellStyle = dataGridViewCellStyle1;
+            this.output.Enabled = false;
+            this.output.Location = new System.Drawing.Point(31, 40);
+            this.output.Margin = new System.Windows.Forms.Padding(0);
+            this.output.Name = "output";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.output.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.output.RowHeadersVisible = false;
+            this.output.RowHeadersWidth = 80;
+            this.output.Size = new System.Drawing.Size(566, 475);
+            this.output.TabIndex = 3;
+            // 
+            // time_txtbox
+            // 
+            this.time_txtbox.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time_txtbox.Location = new System.Drawing.Point(406, 518);
+            this.time_txtbox.Name = "time_txtbox";
+            this.time_txtbox.ReadOnly = true;
+            this.time_txtbox.Size = new System.Drawing.Size(191, 27);
+            this.time_txtbox.TabIndex = 4;
+            this.time_txtbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 548);
+            this.Controls.Add(this.time_txtbox);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.btn_solve);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Eight queen problem";
+            ((System.ComponentModel.ISupportInitialize)(this.output)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_solve;
+        private System.Windows.Forms.DataGridView output;
+        private System.Windows.Forms.TextBox time_txtbox;
+    }
+}
+
